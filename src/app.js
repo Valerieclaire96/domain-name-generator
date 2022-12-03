3f283579b19adca1eaf66252e69a35fb804e3ab7
Valerieclaire96 <Valerieclaire96@gmail.com>
2022-12-03 02:11:57 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+domain = () => {
+  let pronoun = ["the", "our", "her", "his", "their", "my"];
+  let adj = ["great", "big", "hair", "saucy", "black", "purple", "fresh"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let noun = [
+    "jogger",
+    "racoon",
+    "dog",
+    "peguin",
+    "hotdog",
+    "hotpocket",
+    "ketchup"
+  ];
+  let siteType = [".org", ".com", ".gov", ".tv"];
+  //let generateDomainName = () => {
+  for (let i = 0; i < pronoun.length; i++)
+    for (let a = 0; a < adj.length; a++)
+      for (let b = 0; b < noun.length; b++)
+        for (let c = 0; c < siteType.length; c++)
+          console.log(pronoun[i] + adj[a] + noun[b] + siteType[c]);
 };
+//};
+console.log(domain());
